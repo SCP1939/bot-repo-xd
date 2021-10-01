e1e391430d0c52aaa3e2e467c5842c34cf7d2797
Gátomo <gatomociencia@gmail.com>
2021-10-01 18:09:10 +0200

    ordenar el código

--- src/commands/fun/cat.ts
+++ src/commands/fun/cat.ts
@@ -9,40 +9,45 @@ const cataas = new Cataas();
 export const command: Command = {
 	name: 'cat',
 	description: 'Muestra un gatito guapísimo',
-    aliases: ['kitty', 'cats', 'meow'],
+	aliases: ['kitty', 'cats', 'meow'],
 	usage: '',
 	example: [''],
-
-    indev: 'Release Candidate',
+	indev: 'Release Candidate',
 
 	run: async (client: extClient, msg: Message, args: string[]) => {
-        try {
+		try {
+			// El embed tarda en enviarse
+			const embed = new MessageEmbed()
+				.setTitle('🐱 Miau?')
+				.setFooter('Cargando...')
+				.setColor('ORANGE');
 
-            // El embed tarda en enviarse
-            const embed = new MessageEmbed()
-            .setTitle('🐱 Miau?')
-            .setFooter('Cargando...')
-            .setColor('ORANGE')
-            
-            const message = await msg.channel.send({ embeds: [embed] });
-            
-            const newEmbed = message.embeds[0];
+			const message = await msg.channel.send({ embeds: [embed] });
+
+			const newEmbed = message.embeds[0];
 
 
-            // Sin dudarlo la mejor API (excepto por el ping)
-            const res = await axios.get('https://cataas.com/cat?json=true');
+			// Sin dudarlo la mejor API (excepto por el ping)
+			let t1 = +new Date();
+			const res = await axios.get('https://cataas.com/cat?json=true');
+			let t2 = +new Date();
 
-            newEmbed
-            .setTitle('😻 Aquí tienes un gatito guapísimo')
-            .setImage('https://cataas.com' + res.data.url)
-                .setColor('ORANGE')
-                .setFooter('(disculpen el retraso) Hecho con cataas.com')
 
-            message.react('❤️');
-            
-            return message.edit({embeds: [newEmbed]});
-        } catch (err) {
-            return msg.channel.send('🤔**・Hmmm, parece que algo no salió bien. Vuelve a intentarlo!**')
-        }
-    }
+			// Retornar gato
+			newEmbed
+				.setTitle('😻 Aquí tienes un gatito guapísimo')
+				.setImage('https://cataas.com' + res.data.url)
+				.setColor('ORANGE')
+				.setFooter(`(Cargado en ${t2 - t1} ms.) Hecho con cataas.com`);
+
+			message.react('❤️');
+
+			return message.edit({ embeds: [newEmbed] });
+
+		} catch (err) {
+			return msg.channel.send(
+				'🤔**・Hmmm, parece que algo no salió bien. Vuelve a intentarlo!**'
+			);
+		}
+	}
 }
