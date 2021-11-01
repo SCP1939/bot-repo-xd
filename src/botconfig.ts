b3b3eb33f83a5f2c2af4a059ae7284f4b0ae6401
Gátomo <Gatomociencia@gmail.com>
2021-11-01 13:07:02 +0100

    Cambiar prefix

--- src/botconfig.ts
+++ src/botconfig.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-    prefix: 'a.',
+    prefix: 'n!',
     dev: '774795377999085579',
     mongo: process.env.MONGO_URI,
     token: process.env.TOKEN
